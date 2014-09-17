0b5fc9ed07a7913d7c4d9677b6e08b23948207df
Taras Mankovski <taras@quandl.com>
2014-09-17 07:01:47 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
-# Ember-resize-mixin
+# ember-resize-mixin
 
-This README outlines the details of collaborating on this Ember addon.
+Ember Resize Mixin allows child views to resize after parent view was resized.
+
+## Use
+
+```javascript
+import ResizeMixin from 'ember-resize-mixin/main';
+
+export default View.extend(ResizeMixin, {
+  /**
+   * Trigger this function when parent (or window) is resized. Property name does not matter.
+   */
+  onResize: function() {
+    // do what you want when resize is triggered
+  }.on('resize')
+});
+```
 
 ## Installation
 
---
+++ addon/main.js
@@ -0,0 +1,74 @@
+import Ember from 'ember';
+
+export default Ember.Mixin.create({
+  resizing: false,
+  resizeDelay: 200,
+  /**
+   * Recursively traverse view tree looking for parent element that has a handler bound to
+   */
+  findResizableParentView: function (parent) {
+    if (Ember.isNone(parent)) {
+      return null;
+    }
+    if (parent && !parent.has('resize')) {
+      return this.findResizableParentView(parent.get('parentView'));
+    }
+    return parent;
+  },
+  /**
+   * Bind resizeHandler either to the parent view or window
+   */
+  _setupResizeHandlers: function () {
+    var resizeHandler = this.get('_handleResize');
+    var parent = this.findResizableParentView(this.get('parentView'));
+    if (Ember.isNone(parent)) {
+      resizeHandler = Ember.$.proxy(resizeHandler, this);
+      // element doesn't have resizable views, so bind to the window
+      Ember.$(window).on("resize." + this.elementId, resizeHandler);
+      this._resizeHandler = resizeHandler;
+    } else {
+      parent.on('resize', this, resizeHandler);
+    }
+  }.on('didInsertElement'),
+  /**
+   * Unbind from window if window binding was used
+   */
+  _removeResizeHandlers: function () {
+    if (this._resizeHandler) {
+      Ember.$(window).off("resize." + this.elementId, this._resizeHandler);
+    }
+  }.on('willDestroyElement'),
+  /**
+   * Triggers resize events
+   * Promise allows to chain events so async operations happen in sequence
+   * @param event
+   * @param promise
+   * @private
+   */
+  _handleResize: function (event, promise) {
+    if (Ember.isNone(promise)) {
+      promise = Ember.RSVP.resolve(null, "Resize handler");
+    }
+    if (!this.get('resizing')) {
+      this.set('resizing', true);
+      if (this.has('resizeStart')) {
+        this.trigger('resizeStart', event);
+      }
+    }
+    if (this.has('resize')) {
+      this.trigger('resize', event, promise);
+    }
+    Ember.run.debounce(this, this._endResize, event, this.get('resizeDelay'));
+  },
+  /**
+   * Ends resizing by triggering resizeEnd event
+   * @param event
+   * @private
+   */
+  _endResize: function (event) {
+    this.set('resizing', false);
+    if (this.has('resizeEnd')) {
+      this.trigger('resizeEnd', event);
+    }
+  }
+});
